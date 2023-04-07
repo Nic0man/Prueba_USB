6658d1046ba5a2b28a2df346a024bd2524128bb2
Nic0man <danic_169@hotmail.com>
2023-04-07 18:39:26 -0500

    actualización Calculo

--- QT/Calculo.py
+++ QT/Calculo.py
@@ -8,8 +8,13 @@ class Calculo(PyQt.QWidget):
         self.initGUI()
     
     def initGUI(self):
-        self.setGeometry(300,300,200,300)
+        self.setGeometry(300,300,220,170)
         self.setWindowTitle('First App')
+
+        Titulo = PyQt.QLabel(self)
+        Titulo.setText('Primera APP')
+        Titulo.move(50,5)
+        Titulo.setStyleSheet("QLabel{font-size: 18pt;}")
 
         self.num1 = PyQt.QLineEdit(self)
         self.num1.move(55,50)
@@ -35,23 +40,68 @@ class Calculo(PyQt.QWidget):
 
         boton = PyQt.QPushButton(self)
         boton.setText('Calcular')
-        boton.move(145,90)
+        boton.move(145,130)
         boton.resize(50,25)
+
+        #Vlayout = PyQt.QVBoxLayout()
+
+        self.b1 = PyQt.QRadioButton('Sumar',self)
+        self.b1.setChecked(True)
+        self.b1.move(145,45)
+        #self.b1.clicked.connect(lambda: self.prueba(self.b1))
+        #Vlayout.addWidget(self.b1)
+
+        self.b2 = PyQt.QRadioButton('Restar',self)
+        self.b2.move(145,65)
+        #self.b2.clicked.connect(lambda: self.prueba(self.b2))
+        #Vlayout.addWidget(self.b2)
+        #self.setLayout(Vlayout)
+
+        self.b3 = PyQt.QRadioButton('Multiplicar',self)
+        self.b3.move(145,85)
+        #self.b3.clicked.connect(lambda: self.prueba(self.b3))
+
+        self.b4 = PyQt.QRadioButton('Dividir',self)
+        self.b4.move(145,105)
+        #self.b4.clicked.connect(lambda: self.prueba(self.b4))
 
         self.resultxt = PyQt.QLabel(self)
         self.resultxt.move(55,130)
         self.resultxt.resize(80,25)
-        self.resultxt.setStyleSheet("QLabel {background-color: red;"\
+        self.resultxt.setStyleSheet("QLabel{background-color: red;"\
         "font-weight: bold}")
         self.resultxt.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        boton.clicked.connect(self.suma)
-        self.show()
+        boton.clicked.connect(self.calcular)
 
-    def suma(self):
-        suma = int(self.num1.text())+int(self.num2.text())
-        self.resultxt.setText(str(suma))
+        # boton.clicked.connect(lambda: self.resultxt.setText(str(\
+        #     int(self.num1.text())+int(self.num2.text()))))
+
+        self.show()
         
+    # def prueba(self,b):
+    #     if b.isChecked() == True and b.text() == 'Sumar':
+    #         print (f'{self.b1.text()} is selected')
+    #     elif b.isChecked() == True and b.text() == 'Restar':
+    #         print (f'{self.b2.text()} is selected')
+    #     elif b.isChecked() == True and b.text() == 'Multiplicar':
+    #         print (f'{self.b3.text()} is selected')
+    #     else:
+    #         print (f'{self.b4.text()} is selected')
+
+    def calcular(self):
+        if self.b1.isChecked() == True:
+            self.resultxt.setText(str(\
+                int(self.num1.text())+int(self.num2.text())))
+        elif self.b2.isChecked() == True:
+            self.resultxt.setText(str(\
+                int(self.num1.text())-int(self.num2.text())))
+        elif self.b3.isChecked() == True:
+            self.resultxt.setText(str(\
+                int(self.num1.text())*int(self.num2.text())))
+        else:
+            self.resultxt.setText(str(\
+                int(self.num1.text())/int(self.num2.text())))           
 
 def main():
     app = PyQt.QApplication([])
