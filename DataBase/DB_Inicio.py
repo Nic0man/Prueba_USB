4a46fd6aac379e984f1b195db81482b03d1c41ee
Nic0man <danic_169@hotmail.com>
2023-04-11 01:30:21 -0500

    Uso de base de datos

--- DataBase/DB_Inicio.py
+++ DataBase/DB_Inicio.py
@@ -1,33 +1,55 @@
 import sqlite3
+#http://rogercorp.club/minticc3/
 
-def consulta(correo, password):
-    db=sqlite3.connect('Mensajes.s3db')
-    db.row_factory = sqlite3.Row
-    cursor = db.cursor()
-    consulta = "select * from Usuarios where CorreoUsuario\
-         = '" + correo + "' and Password = '" + password + \
-            "' and Estado = '1'"
+def validar(usuario):
+    db=sqlite3.connect("datos.s3db")
+    db.row_factory=sqlite3.Row
+    cursor=db.cursor()
+    consulta="select * from Usuarios where apellido='" + usuario +"'"
+    #consulta="select * from Usuarios"
     cursor.execute(consulta)
-    resultado = cursor.fetchall()
+    resultado=cursor.fetchall()
+    for i in resultado:
+        print(list(i))
+    # for i in resultado:
+    #     for j in i:
+    #         print(j,end=' ')
+    #     print('\n')
+    cursor.close()
+    db.close()
     return resultado
 
+def validar2(usuario):
+    db=sqlite3.connect("datos.s3db")
+    db.row_factory=sqlite3.Row
+    cursor=db.cursor()
+    consulta="select * from Usuarios where apellido = '" + usuario +"'"
+    cursor.execute(consulta)
+    resultado=cursor.fetchone()
+    print(list(resultado))
+    # for i in resultado:
+    #     print(i)
+    
+    cursor.close()
+    db.close()
+    return resultado
 
-def manipulacion(Usuario,correo, password,codigo):
-    db=sqlite3.connect('Mensajes.s3db')
+def Registrar(nombre,apellido,edad):
+    db = sqlite3.connect("datos.s3db")
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
-    consulta = "insert into Usuarios (NombreUsuario, \
-        CorreoUsuario, Password, Estado, CodigoActivacion)\
-             values ('"+ Usuario +"','" + correo + "','" \
-                + password + "','0','"+ codigo +"')"
+    consulta = "insert into Usuarios (nombre,apellido,edad) values ('"+ nombre +"','" + apellido + "'," + str(edad) +")"
     cursor.execute(consulta)
     db.commit()
+    cursor.close()
+    db.close()
     return "1"
 
 
 def main():
-    consulta('David','david@usbbog.edu.co','123456','1')
-    manipulacion('david@usbbog.edu.co','123456')
+    #Registrar('Alfredo','Torres',62)
+    validar('Torres')
+    validar2('Torres')
 
 
 if __name__ == "__main__":
